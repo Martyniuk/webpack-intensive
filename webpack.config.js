6da31a1635fc48b80d09aeb51ece8157fc9d19a1
Dima Vakatsiienko <d.vakatsiienko@lectrum.io>
2018-10-15 19:37:41 +0300

    → Checkpoint at: 21:23:19

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             title: 'Learn Webpack very well',
             template: './static/template.html',
-            favicon: './static/favicon.ico'
+            favicon: './static/favicon.ico',
         }),
     ],
 };
