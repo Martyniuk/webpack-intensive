3f08c7a7a3551152d7f7015b749f0eaf51d1699b
Dima Vakatsiienko <d.vakatsiienko@lectrum.io>
2018-10-15 19:37:41 +0300

    → Checkpoint at: 21:03:52

--- webpack.config.js
+++ webpack.config.js
@@ -4,4 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     mode: 'development',
     devtool: 'false',
+    plugins: [
+        new HtmlWebpackPlugin()
+    ]
 };
