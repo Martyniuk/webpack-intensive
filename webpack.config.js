6c790b6b32f66533e4719b765fa7617396e381ba
Dima Vakatsiienko <d.vakatsiienko@lectrum.io>
2018-10-15 19:37:41 +0300

    → Checkpoint at: 21:28:43

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
 // Core
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = Promise.resolve({
-    mode: 'development',
-    devtool: 'false',
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Learn Webpack very well',
-            template: './static/template.html',
-            favicon: './static/favicon.ico',
-        }),
-    ],
-});
+module.exports = () => {
+    return {
+        mode: 'development',
+        devtool: 'false',
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: 'Learn Webpack very well',
+                template: './static/template.html',
+                favicon: './static/favicon.ico',
+            }),
+        ],
+    };
+};
