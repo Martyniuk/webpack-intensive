e43d08e92ef9c910eac2ce96ca8be6570509991d
Dima Vakatsiienko <d.vakatsiienko@lectrum.io>
2018-10-15 19:37:41 +0300

    → Checkpoint at: 21:18:07

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,7 @@ module.exports = {
     devtool: 'false',
     plugins: [
         new HtmlWebpackPlugin({
-            template: require('html-webpack-template'),
-            appMountId: 'app',
             title: 'Learn Webpack very well',
-            mobile: true,
         }),
     ],
 };
