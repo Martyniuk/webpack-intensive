21f227b4049ed69b3a37095683859d6af6115f5c
Dima Shvedsky <imagnum.satellite@gmail.com>
2018-10-22 19:25:17 +0300

    → Checkpoint at: 20:06:19

---
+++ scripts/build.1.js
@@ -0,0 +1,47 @@
+// Core
+const webpack = require('webpack');
+const chalk = require('chalk');
+
+// Config
+const getConfig = require('./webpack.config');
+
+const compiler = webpack(getConfig());
+
+compiler.run((error, stats) => {
+    if (error) {
+        console.error(error.stack || error);
+
+        if (error.details) {
+            console.error(error.details);
+        }
+
+        return;
+    }
+
+    const info = stats.toString({
+        colors:     true,
+        hash:       true,
+        version:    true,
+        timings:    true,
+        env:        true,
+        chunks:     false,
+        modules:    false,
+        children:   false,
+        publicPath: true,
+        reasons:    true,
+        source:     false,
+    });
+
+    console.log(info);
+    console.log(chalk.greenBright('✓ Build completed.'));
+
+    if (stats.hasErrors()) {
+        console.log(chalk.redBright('→ Error!'));
+        console.error(info);
+    }
+
+    if (stats.hasWarnings()) {
+        console.log(chalk.yellowBright('→ Warning!'));
+        console.warn(info);
+    }
+});
