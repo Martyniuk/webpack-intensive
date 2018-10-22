d97fbd77182680aa85ea4da3b356c3979542c8e0
Dima Shvedsky <imagnum.satellite@gmail.com>
2018-10-22 19:25:17 +0300

    → Checkpoint at: 21:18:21

---
+++ scripts/_start.1.js
@@ -0,0 +1,39 @@
+// Core
+const webpack = require('webpack');
+// const MemFs = require('memory-fs');
+const chalk = require('chalk');
+
+// Config
+const getConfig = require('./webpack.config');
+// const memFs = new MemFs();
+
+const compiler = webpack(getConfig());
+
+// compiler.outputFileSystem = memFs;
+
+compiler.watch({ ignored: [ 'node_modules' ] }, (error, stats) => {
+    console.log(chalk.greenBright('✓ webpack is watching...'));
+    if (error) {
+        console.error(error.stack || error);
+
+        if (error.details) {
+            console.error(error.details);
+        }
+
+        return;
+    }
+
+    const info = stats.toString('errors-only');
+
+    console.log(info);
+
+    if (stats.hasErrors()) {
+        console.log(chalk.redBright('→ Error!'));
+        console.error(info);
+    }
+
+    if (stats.hasWarnings()) {
+        console.log(chalk.yellowBright('→ Warning!'));
+        console.warn(info);
+    }
+});
