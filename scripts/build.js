7ab90fc276d559d728f977d4e814f6c87d8081e5
Dima Shvedsky <imagnum.satellite@gmail.com>
2018-10-22 19:25:17 +0300

    → Checkpoint at: 19:50:29

--- scripts/build.js
+++ scripts/build.js
@@ -1,1 +1,11 @@
-console.log('→ message');
+// Core
+const webpack = require('webpack');
+
+const getConfig = require('./webpack.config');
+
+const compiler = webpack(getConfig());
+
+compiler.run(() => {
+    // ...
+    console.log('→ message');
+})
