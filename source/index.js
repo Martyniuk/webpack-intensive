fc963940366fa5ee1ab16e833691b9532c5ab2d2
Dima Shvedsky <imagnum.satellite@gmail.com>
2018-10-22 19:25:17 +0300

    → Checkpoint at: 21:13:02

--- source/index.js
+++ source/index.js
@@ -2,3 +2,5 @@ import './theme/init.css';
 import component from './simple-components/dom';
 
 document.body.appendChild(component());
+
+// express + webpack-dev-middleware + webpack-hot-middleware = webpack-dev-server
