18942b4d5df6549a6548dcf79be59fd8df2a36a7
Dima Shvedsky <imagnum.satellite@gmail.com>
2018-10-22 19:25:17 +0300

    → Checkpoint at: 21:42:42

--- source/simple-components/dom.js
+++ source/simple-components/dom.js
@@ -1,20 +1,7 @@
-const component = (text = 'Привет!') => {
+export default (text = 'Привет!') => {
     const element = document.createElement('div');
 
     element.innerHTML = text;
 
     return element;
 };
-
-let element = component(); // Store the element to re-render on print.js changes
-document.body.appendChild(element);
-
-if (module.hot) {
-    module.hot.accept('./print.js', function() {
-        console.log('Accepting the updated printMe module!');
-        printMe();
-        document.body.removeChild(element);
-        element = component(); // Re-render the "component" to update the click handler
-        document.body.appendChild(element);
-    });
-}
