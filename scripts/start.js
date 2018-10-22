8d4c30af6897caae6c4f0f3e2cd9be42e06859a9
Dima Shvedsky <imagnum.satellite@gmail.com>
2018-10-22 19:25:17 +0300

    → Checkpoint at: 21:46:11

--- scripts/start.js
+++ scripts/start.js
@@ -3,7 +3,6 @@ const webpack = require('webpack');
 const devServer = require('webpack-dev-server');
 const hot = require('webpack-hot-middleware');
 const chalk = require('chalk');
-const { resolve } = require('path');
 
 // Config
 const getConfig = require('./webpack.config');
