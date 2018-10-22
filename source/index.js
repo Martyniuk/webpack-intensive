4d0b2713f916ec3277f7932a979e21f995f73462
Dima Shvedsky <imagnum.satellite@gmail.com>
2018-10-22 19:25:17 +0300

    → Checkpoint at: 21:42:06

--- source/index.js
+++ source/index.js
@@ -2,3 +2,16 @@ import './theme/init.css';
 import component from './simple-components/dom';
 
 document.body.appendChild(component());
+
+let element = component(); // Store the element to re-render on print.js changes
+document.body.appendChild(element);
+
+if (module.hot) {
+    module.hot.accept('./print.js', function() {
+        console.log('Accepting the updated printMe module!');
+        printMe();
+        document.body.removeChild(element);
+        element = component(); // Re-render the "component" to update the click handler
+        document.body.appendChild(element);
+    });
+}
