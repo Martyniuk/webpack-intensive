20cb7e291a1bdc34f7b999cb2c796134dc50b234
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2018-10-26 19:38:05 +0300

    → Checkpoint at: 20:34:48

--- .babelrc.js
+++ .babelrc.js
@@ -8,9 +8,10 @@ module.exports = (api) => {
         presets: [
             [
                 '@babel/preset-env',
-                // {
-
-                // }
+                {
+                    spec: true, // медленней, более строго 
+                    loose: true, // быстрей, менее строго
+                }
             ]
         ]
     }
