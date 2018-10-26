d79b24086da89e3dfdd3610bb58f9af84ea0c23e
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2018-10-26 19:38:05 +0300

    → Checkpoint at: 21:03:07

--- source/components/index.js
+++ source/components/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { hot } from 'react-hot-loader';
 
-const Test = hot(
+const Test = hot(module)(
     class extends Component {
         render() {
             console.log('→ приветик');
