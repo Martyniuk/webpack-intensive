f164f067a23e45d70e53b3b214f9fc3bb502b9e2
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2018-10-26 19:38:05 +0300

    → Checkpoint at: 20:03:36

--- scripts/webpack/config/webpack.prod.js
+++ scripts/webpack/config/webpack.prod.js
@@ -1,16 +1,16 @@
 // Core
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const merge = require('webpack-merge');
 
 // Instruments
 const { SOURCE, PROJECT_ROOT } = require('../constants');
+const getCommonConfig = require('./webpack.common');
 
-module.exports = () => {
-    return {
-        entry:   SOURCE,
-        plugins: [
-            new CleanWebpackPlugin([ 'build' ], {
-                root: PROJECT_ROOT,
-            }),
-        ],
-    };
-};
+module.exports = () => merge(getCommonConfig(), {
+    entry:   SOURCE,
+    plugins: [
+        new CleanWebpackPlugin([ 'build' ], {
+            root: PROJECT_ROOT,
+        }),
+    ],
+});
