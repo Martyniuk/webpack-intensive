bfe6d5676fe50d34260abae5f89a4f2eb1b12523
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2018-10-26 19:38:05 +0300

    → Checkpoint at: 19:41:55

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,3 +1,12 @@
+// Core
+const { path: PROJECT_ROOT } = require('app-root-path');
+const { resolve } = require('path');
+
 // Network
 exports.HOST = 'localhost';
 exports.PORT = 3000;
+
+// Paths
+exports.PROJECT_ROOT = PROJECT_ROOT;
+exports.SOURCE = resolve(PROJECT_ROOT, './source');
+exports.BUILD = resolve(PROJECT_ROOT, './build');
