73d5e1e1d2b8440c8299eac0e90e2262e4373763
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2018-10-26 19:38:05 +0300

    → Checkpoint at: 19:47:21

--- scripts/webpack/build.js
+++ scripts/webpack/build.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 const chalk = require('chalk');
 
 // Config
-const getConfig = require('./webpack.config');
+const getConfig = require('./config/webpack.config');
 
 const compiler = webpack(getConfig());
 
