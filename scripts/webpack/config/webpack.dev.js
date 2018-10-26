ff8ee18f35905627b9450487d2711bfcd989060f
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2018-10-26 19:38:05 +0300

    → Checkpoint at: 21:42:23

--- scripts/webpack/config/webpack.dev.js
+++ scripts/webpack/config/webpack.dev.js
@@ -1,15 +1,24 @@
 // Core
 const { HotModuleReplacementPlugin } = require('webpack');
 const merge = require('webpack-merge');
+const { choosePort } = require('react-dev-utils');
 
 // Instruments
-const { SOURCE } = require('../constants');
+const { SOURCE, HOST, PORT } = require('../constants');
 const getCommonConfig = require('./webpack.common');
 
-module.exports = () => {
+module.exports = async () => {
+    const suggestedPort = await choosePort(HOST, PORT);
+
     return merge(getCommonConfig(), {
         mode:    'development',
         entry:   [ 'webpack-hot-middleware/client?reload=true&quiet=true', SOURCE ],
+        {
+            devServer: {
+                host: HOST,
+                port: suggestedPort
+            }
+        }
         plugins: [ new HotModuleReplacementPlugin() ],
     });
 };
