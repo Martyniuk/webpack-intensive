e7c900dfdfc123609a327f971a589c61fff9e8f0
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2018-10-26 19:38:05 +0300

    → Checkpoint at: 21:25:56

--- scripts/webpack/config/webpack.common.js
+++ scripts/webpack/config/webpack.common.js
@@ -27,7 +27,12 @@ module.exports = () => {
                     test: /\.css$/,
                     use:  [
                         'style-loader',
-                        'css-loader',
+                        {
+                            loader:  'css-loader',
+                            options: {
+                                importLoaders: 1,
+                            },
+                        },
                         {
                             loader:  'postcss-loader',
                             options: {
