db86ed3a1371599267448679be0e104d58304ad6
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2018-10-26 19:38:05 +0300

    → Checkpoint at: 19:38:06

--- source/index.js
+++ source/index.js
@@ -1,14 +1,14 @@
 import './theme/init.css';
 import component from './simple-components/dom';
 
-let element = component(); // Store the element to re-render on print.js changes
+let element = component();
 
 document.body.appendChild(element);
 
 if (module.hot) {
     module.hot.accept('./simple-components/dom', function() {
         document.body.removeChild(element);
-        element = component(); // Re-render the "component" to update the click handler
+        element = component();
         document.body.appendChild(element);
     });
 }
