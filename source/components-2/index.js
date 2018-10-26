1d9ef8ba62656b3df30316ace745339b09da27f7
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2018-10-26 19:38:05 +0300

    → Checkpoint at: 21:35:13

--- source/components-2/index.js
+++ source/components-2/index.js
@@ -1,22 +1,17 @@
 import React, { Component } from 'react';
-import { render } from 'react-dom';
 import { hot } from 'react-hot-loader';
 
 import Styles from './styles.css';
 
-const Test = hot(module)(
-    class extends Component {
-        render() {
-            console.log('→ приветик');
+export class Test2 extends Component {
+    render() {
+        console.log('→ приветик');
 
-            return (
-                <>
-                    <h1 className = { Styles.hello }>Хелоу!</h1>
-                    <h1 className = { Styles.hello }>Хелоу!</h1>
-                </>
-            );
-        }
-    },
-);
-
-render(<Test />, document.getElementById('app'));
+        return (
+            <>
+                <h1 className = { Styles.hello }>Хелоу!</h1>
+                <h1 className = { Styles.hello }>Хелоу!</h1>
+            </>
+        );
+    }
+}
