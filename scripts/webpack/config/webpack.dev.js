d100e991be637ce2bd00f788b2324f15fc2e04fc
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2018-10-26 19:38:05 +0300

    → Checkpoint at: 19:54:02

--- scripts/webpack/config/webpack.dev.js
+++ scripts/webpack/config/webpack.dev.js
@@ -2,20 +2,12 @@
 const { HotModuleReplacementPlugin } = require('webpack');
 
 // Instruments
-const { SOURCE, BUILD } = require('../constants');
+const { SOURCE } = require('../constants');
 
 module.exports = () => {
     return {
-        mode:   'development',
-        entry:  [ 'webpack-hot-middleware/client?reload=true&quiet=true', SOURCE ],
-        module: {
-            rules: [
-                {
-                    test: /\.css$/,
-                    use:  [ 'style-loader', 'css-loader' ],
-                },
-            ],
-        },
+        mode:    'development',
+        entry:   [ 'webpack-hot-middleware/client?reload=true&quiet=true', SOURCE ],
         plugins: [ new HotModuleReplacementPlugin() ],
     };
 };
