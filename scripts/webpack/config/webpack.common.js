6b75f2a9a662964ccc2223851687736e57885f6d
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2018-10-26 19:38:05 +0300

    → Checkpoint at: 21:33:36

--- scripts/webpack/config/webpack.common.js
+++ scripts/webpack/config/webpack.common.js
@@ -31,7 +31,7 @@ module.exports = () => {
                             loader:  'css-loader',
                             options: {
                                 importLoaders: 1,
-                                modules:       false,
+                                modules:       true,
                             },
                         },
                         {
