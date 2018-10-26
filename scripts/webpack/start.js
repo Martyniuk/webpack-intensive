eb3988e3b52fa8ba17b243d40b15ba5d90320d7e
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2018-10-26 19:38:05 +0300

    → Checkpoint at: 19:48:31

--- scripts/webpack/start.js
+++ scripts/webpack/start.js
@@ -5,7 +5,7 @@ const hot = require('webpack-hot-middleware');
 const chalk = require('chalk');
 
 // Config
-const getConfig = require('./config/webpack.config');
+const getConfig = require('./config/webpack.common');
 
 // Constants
 const { HOST, PORT } = require('./constants');
