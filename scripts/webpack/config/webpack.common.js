33b653e739e277fca0b9d4e3232f4dd766bd73d2
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2018-10-26 19:38:05 +0300

    → Checkpoint at: 21:13:53

--- scripts/webpack/config/webpack.common.js
+++ scripts/webpack/config/webpack.common.js
@@ -22,10 +22,10 @@ module.exports = () => {
                         },
                     },
                 },
-                // {
-                //     test: /\.css$/,
-                //     use:  [ 'style-loader', 'css-loader' ],
-                // },
+                {
+                    test: /\.css$/,
+                    use:  [ 'style-loader', 'css-loader' ],
+                },
             ],
         },
         plugins: [
