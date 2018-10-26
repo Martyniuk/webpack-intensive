4a5181096e01b3366f625bf85cfb1676c611032f
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2018-10-26 19:38:05 +0300

    → Checkpoint at: 21:30:58

--- source/index.js
+++ source/index.js
@@ -1,23 +1,23 @@
 import './theme/init.css';
 
-// import './components';
+import './components';
 
 /* javascript */
 // import './simple-components/javascript';
 /* javascript */
 
 /* dom */
-import component from './simple-components/dom';
+// import component from './simple-components/dom';
 
-let element = component();
+// let element = component();
 
-document.body.appendChild(element);
+// document.body.appendChild(element);
 
-if (module.hot) {
-    module.hot.accept('./simple-components/dom', function() {
-        document.body.removeChild(element);
-        element = component();
-        document.body.appendChild(element);
-    });
-}
+// if (module.hot) {
+//     module.hot.accept('./simple-components/dom', function() {
+//         document.body.removeChild(element);
+//         element = component();
+//         document.body.appendChild(element);
+//     });
+// }
 /* dom */
