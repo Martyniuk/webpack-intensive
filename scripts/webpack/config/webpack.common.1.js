0d65b7a1d9ef35ecebf47edbeef13ed38a90fe91
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2018-10-26 19:38:05 +0300

    → Checkpoint at: 19:54:22

---
+++ scripts/webpack/config/webpack.common.1.js
@@ -0,0 +1,35 @@
+// Core
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+// Instruments
+const { SOURCE, BUILD, PROJECT_ROOT } = require('../constants');
+
+module.exports = () => {
+    return {
+        entry:  SOURCE,
+        output: {
+            path:     BUILD,
+            filename: 'bundle.js',
+        },
+        devtool: false,
+        module:  {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use:  [ 'style-loader', 'css-loader' ],
+                },
+            ],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                title:    'Learn Webpack very well',
+                template: './static/template.html',
+                favicon:  './static/favicon.ico',
+            }),
+            new CleanWebpackPlugin([ 'build' ], {
+                root: PROJECT_ROOT,
+            }),
+        ],
+    };
+};
