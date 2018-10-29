45ad85a7e585a0e6dd9c0aee585d78a0b6e0d39e
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2018-10-29 18:45:18 +0200

    → Lesson 4, checkpoint at: 20:53:37

---
+++ scripts/webpack/modules/assets.js
@@ -0,0 +1,27 @@
+export const loadFonts = () => ({
+    module: {
+        rules: [
+            {
+                test: /\.woff2$/,
+                use:  [
+                    {
+                        loader:  'file-loader',
+                        options: {
+                            name: `./fonts/${CHUNK_NAME_ASSET}`,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+});
+
+export const connectHtml = () => ({
+    plugins: [
+        new HtmlWebpackPlugin({
+            title:    'Learn Webpack',
+            template: `${STATIC}/template.html`,
+            favicon:  `${STATIC}/favicon.ico`,
+        }),
+    ],
+});
