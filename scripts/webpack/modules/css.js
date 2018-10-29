d0b286dc6c358624825729d22d799ae747deb8f0
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2018-10-29 18:45:18 +0200

    → Lesson 4, checkpoint at: 20:02:08

---
+++ scripts/webpack/modules/css.js
@@ -0,0 +1,38 @@
+export const loadCss = () => {
+    return {
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use:  [
+                        'style-loader',
+                        {
+                            loader:  'css-loader',
+                            options: {
+                                importLoaders:  1,
+                                modules:        true,
+                                localIdentName:
+                                    '[path][name]__[local]--[hash:base64:5]',
+                            },
+                        },
+                        {
+                            loader:  'postcss-loader',
+                            options: {
+                                plugins: [
+                                    env({
+                                        // plugin 1
+                                        stage: 0,
+                                    }),
+                                    // plugin 2
+                                    // plugin 3
+                                    // plugin n
+                                    // last plugin cssnano → minify
+                                ],
+                            },
+                        },
+                    ],
+                },
+            ],
+        },
+    };
+};
