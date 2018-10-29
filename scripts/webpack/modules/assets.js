bdc6dec573b18e26d048b343af7aebdb0f3896cd
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2018-10-29 18:45:18 +0200

    → Lesson 4, checkpoint at: 21:33:22

--- scripts/webpack/modules/assets.js
+++ scripts/webpack/modules/assets.js
@@ -24,6 +24,43 @@ export const loadImages = () => ({
     },
 });
 
+export const loadSvg = () => ({
+    module: {
+        rules: [
+            {
+                test:   /\.svg$/,
+                issuer: {
+                    test: /\.js$/,
+                },
+                use: [
+                    '@svgr/webpack',
+                    {
+                        loader:  'file-loader',
+                        options: {
+                            name: `./images/[name].[ext]`,
+                        },
+                    },
+                ],
+            },
+            {
+                test:   /\.svg$/,
+                issuer: {
+                    test: /\.css$/,
+                },
+                use: [
+                    {
+                        loader:  'file-loader',
+                        options: {
+                            // limit: 0,
+                            name: `./images/[name].[ext]`,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+});
+
 export const loadFonts = () => ({
     module: {
         rules: [
