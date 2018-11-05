15dd7a15eee2517f00c2552d2a731e273a65f8af
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2018-11-05 18:38:25 +0200

    → Lesson 6, checkpoint at: 19:05:54

--- scripts/webpack/config/webpack.dev.js
+++ scripts/webpack/config/webpack.dev.js
@@ -1,23 +1,29 @@
 // Core
-import { HotModuleReplacementPlugin } from 'webpack';
 import merge from 'webpack-merge';
 import { choosePort } from 'react-dev-utils/WebpackDevServerUtils';
 
 // Instruments
 import { SOURCE, HOST, PORT } from '../constants';
 import getCommonConfig from './webpack.common';
+import { connectHotModuleReplacement } from '../modules';
 
 export default async () => {
     const suggestedPort = await choosePort(HOST, PORT);
 
-    return merge(getCommonConfig(), {
-        mode:      'development',
-        entry:     [ 'webpack-hot-middleware/client?reload=true&quiet=true', SOURCE ],
-        devServer: {
-            host: HOST,
-            port: suggestedPort,
+    return merge(
+        getCommonConfig(),
+        {
+            mode:  'development',
+            entry: [
+                'webpack-hot-middleware/client?reload=true&quiet=true',
+                SOURCE,
+            ],
+            devServer: {
+                host: HOST,
+                port: suggestedPort,
+            },
+            devtool: 'cheap-module-eval-source-map',
         },
-        devtool: 'cheap-module-eval-source-map',
-        plugins: [ new HotModuleReplacementPlugin() ],
-    });
+        connectHotModuleReplacement(),
+    );
 };
