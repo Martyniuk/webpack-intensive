a58163d05b0f0e441f2052d7946f42dceff5a06b
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2018-11-05 18:38:25 +0200

    → Lesson 6, checkpoint at: 21:12:29

--- source/index.js
+++ source/index.js
@@ -21,13 +21,13 @@ let element = component();
 
 document.body.appendChild(element);
 
-// if (module.hot) {
-//     module.hot.accept('./simple-components/dom', function() {
-//         document.body.removeChild(element);
-//         element = component();
-//         document.body.appendChild(element);
-//     });
-// }
+if (module.hot) {
+    module.hot.accept('./simple-components/dom', function() {
+        document.body.removeChild(element);
+        element = component();
+        document.body.appendChild(element);
+    });
+}
 /* dom */
 
 /* Env variables */
