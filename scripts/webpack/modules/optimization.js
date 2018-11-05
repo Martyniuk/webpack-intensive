ba82c860c4ccaffab4acada0758a3cb83d1ce180
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2018-11-05 18:38:25 +0200

    → Lesson 6, checkpoint at: 20:42:19

--- scripts/webpack/modules/optimization.js
+++ scripts/webpack/modules/optimization.js
@@ -1,5 +1,9 @@
 // Core
 import UglifyJsPlugin from 'uglifyjs-webpack-plugin';
+import { ImageminWebpackPlugin } from 'imagemin-webpack';
+import imageminMozjpeg from 'imagemin-mozjpeg';
+import imageminPngquant from 'imagemin-pngquant';
+import imageminSvgo from 'imagemin-svgo';
 
 export const optimizeModules = () => ({
     optimization: {
@@ -49,3 +53,22 @@ export const optimizeModules = () => ({
 //    →→→→→→   chunk2
 
 // mangle — увечья
+
+export const optimizeImages = () => ({
+    plugins: [
+        new ImageminWebpackPlugin({
+            imageminOptions: {
+                plugins: [
+                    imageminMozjpeg({
+                        progressive: true,
+                        quality:     60,
+                    }),
+                    imageminPngquant({
+                        quality: 60,
+                    }),
+                    imageminSvgo(),
+                ],
+            },
+        }),
+    ],
+});
