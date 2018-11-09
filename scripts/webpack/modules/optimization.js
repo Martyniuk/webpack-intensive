bff7c660896ed8fc045fa9b280e73969ea1ecd93
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2018-11-09 19:38:32 +0200

    → Lesson 7, checkpoint at: 20:36:30

--- scripts/webpack/modules/optimization.js
+++ scripts/webpack/modules/optimization.js
@@ -77,7 +77,6 @@ export const optimizeModules = () => ({
                     chunks:   'initial',
                     // Выбирает модули, внесённые в данную кеш-группу. Если не указать будут выбраны все модули.
                     test:     /[\\/]node_modules[\\/]/,
-                    name:     'test',
                     priority: -10,
                 },
                 default: {
