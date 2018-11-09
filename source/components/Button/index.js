7d79b0f66ba3804173dd9ddb26c0eadddbe01fb9
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2018-11-09 19:38:32 +0200

    → Lesson 7, checkpoint at: 20:21:42

--- source/components/Button/index.js
+++ source/components/Button/index.js
@@ -4,6 +4,8 @@ import React, { Component } from 'react';
 // Styles
 import Styles from './styles.css';
 
+// import { log } from '../helpers'
+
 export class Button extends Component {
     render() {
         const { text, onClick } = this.props;
