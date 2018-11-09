05040e50c1a5cddf0a150084c1606dac807eed6f
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2018-11-09 19:38:32 +0200

    → Lesson 7, checkpoint at: 20:52:55

--- source/components/Carousel/Alien/index.js
+++ source/components/Carousel/Alien/index.js
@@ -8,7 +8,7 @@ import { Loading } from '../../Loading';
 const LoadableComponent = Loadable({
     loader:  () => import(/* webpackChunkName: "alien" */ './Component'),
     loading: Loading,
-    delay:   2000,
+    // delay:   2000,
 });
 
 export class Alien extends Component {
