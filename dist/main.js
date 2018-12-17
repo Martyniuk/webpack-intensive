50ef42f92f6102ed0043824aa46efedbb0261be3
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2018-12-17 18:34:56 +0200

    → Lesson 1, checkpoint at: 20:59:37

--- dist/main.js
+++ dist/main.js
@@ -81,30 +81,20 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = "./src/index.js");
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
 /******/ })
 /************************************************************************/
-/******/ ({
-
-/***/ "./src/index.js":
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
-/*! no exports provided */
+/******/ ([
+/* 0 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _simple_components_dom__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./simple-components/dom */ "./src/simple-components/dom.js");
+/* harmony import */ var _simple_components_dom__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
 
 
 /***/ }),
-
-/***/ "./src/simple-components/dom.js":
-/*!**************************************!*\
-  !*** ./src/simple-components/dom.js ***!
-  \**************************************/
-/*! exports provided: default */
+/* 1 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -119,5 +109,4 @@ __webpack_require__.r(__webpack_exports__);
 
 
 /***/ })
-
-/******/ });
+/******/ ]);
