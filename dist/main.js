cdfd507a916eb3505aaea1f2ef5548eb9e3b364a
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2018-12-17 18:34:56 +0200

    → Lesson 1, checkpoint at: 20:31:42

--- dist/main.js
+++ dist/main.js
@@ -93,7 +93,7 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-eval("console.log('hello webpack');\n\n//# sourceURL=webpack:///./src/index.js?");
+console.log('hello webpack');
 
 /***/ })
 
