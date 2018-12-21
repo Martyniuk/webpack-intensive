33705074bf7195ecd042ef6528daf4e17f6fb3ea
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2018-12-21 19:26:37 +0200

    → Lesson 2, checkpoint at: 19:53:07

--- scripts/webpack/build.js
+++ scripts/webpack/build.js
@@ -4,3 +4,36 @@ const chalk = require('chalk');
 
 // Config
 const config = require('./webpack.config');
+
+const compiler = webpack(config);
+
+compiler.run((error, stats) => {
+    if (error) {
+        // ошибка конфигурации
+        console.error(error.stack || error);
+
+        if (error.details) {
+            console.error(error.details);
+        }
+
+        return;
+    }
+
+    const info = stats.toString({
+        colors: true,
+        hash: true,
+        version: true,
+        timings: true,
+        env: true,
+        chunks: false,
+        modules: false,
+        children: false,
+        publicPath: true,
+        reasons: true,
+        source: false,
+    });
+
+
+})
+
+// compiler.hooks
