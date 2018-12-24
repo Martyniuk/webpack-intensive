69a70dd04f73645df5647663f39e29934d3022be
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2018-12-24 19:29:42 +0200

    → Lesson 3, checkpoint at: 21:01:16

--- .babelrc.js
+++ .babelrc.js
@@ -1,11 +1,12 @@
 module.exports = api => {
     const env = api.env();
 
-    api.cache.never(); 
+    api.cache.never();
     // api.cache(() => env === 'development');
 
     return {
         presets: [
+            '@babel/preset-react',
             [
                 '@babel/preset-env',
                 {
