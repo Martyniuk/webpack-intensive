9e4087096bb36b824899bd12a7b1fa47a861a4bd
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2018-12-24 19:29:42 +0200

    → Lesson 3, checkpoint at: 21:14:01

--- scripts/webpack/config/webpack.common.js
+++ scripts/webpack/config/webpack.common.js
@@ -19,7 +19,15 @@ module.exports = () => {
                 },
                 {
                     test: /\.css$/,
-                    use:  [ 'style-loader', 'css-loader' ],
+                    use:  [
+                        'style-loader',
+                        {
+                            loader:  'css-loader',
+                            options: {
+                                modules: true,
+                            },
+                        },
+                    ],
                 },
             ],
         },
