a3135506b318aa386fb86290cd4b99714834effe
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2018-12-24 19:29:42 +0200

    → Lesson 3, checkpoint at: 20:06:55

---
+++ source/javascript.js
@@ -0,0 +1,30 @@
+class Human {
+    KIND = 'HUMAN';
+
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    sayName() {
+        console.log('Name', this.name);
+    }
+
+    sayAge() {
+        console.log('Age', this.age);
+    }
+
+    sayKind = () => {
+        console.log('Kind', this.KIND);
+    };
+}
+
+const gendalf = new Human('Gendalf', 666);
+
+gendalf.sayAge();
+gendalf.sayKind();
+
+export const a = 1;
+export const b = 5;
+const c = a + b;
+console.log(c);
