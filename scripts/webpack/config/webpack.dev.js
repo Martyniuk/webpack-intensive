87edffb3e92c1955d5def4e93ca654e028839c93
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2018-12-24 19:29:42 +0200

    → Lesson 3, checkpoint at: 20:02:30

--- scripts/webpack/config/webpack.dev.js
+++ scripts/webpack/config/webpack.dev.js
@@ -7,9 +7,8 @@ const getCommonConfig = require('./webpack.common');
 
 module.exports = () => {
     return merge(getCommonConfig(), {
-        entry: [ 'webpack-hot-middleware/client?reload=true&quiet=true' ],
-        mode:  'development',
-
+        entry:   [ 'webpack-hot-middleware/client?reload=true&quiet=true' ],
+        mode:    'development',
         devtool: false, // TODO: настроить source map
         plugins: [ new HotModuleReplacementPlugin() ],
     });
