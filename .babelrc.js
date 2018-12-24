1d5af959107cae97ef4eebcd45334c818d215174
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2018-12-24 19:29:42 +0200

    → Lesson 3, checkpoint at: 20:20:56

--- .babelrc.js
+++ .babelrc.js
@@ -7,6 +7,7 @@ module.exports = api => {
                 '@babel/preset-env',
                 {
                     debug: true,
+                    useBuiltIns: 'usage',
                 },
             ],
         ],
