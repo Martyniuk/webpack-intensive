b52302ffedd10d3972861fe506dc6f0c5dcd966b
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2018-12-24 19:29:42 +0200

    → Lesson 3, checkpoint at: 20:55:43

---
+++ source/react-component/index.js
@@ -0,0 +1,39 @@
+// Core
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { hot } from 'react-hot-loader';
+
+// Styles
+import Styles from './postcss.css';
+
+const Clicker = hot(module)(
+    class extends Component {
+        state = {
+            count: 0,
+        };
+
+        inc = () =>
+            void this.setState(({ count }) => ({
+                count: count + 1,
+            }));
+
+        dec = () =>
+            void this.setState(({ count }) => ({
+                count: count - 1,
+            }));
+
+        render() {
+            const { count } = this.state;
+
+            return (
+                <section className={Styles.clicker}>
+                    <h1>Count: {count}</h1>
+                    <button onClick={this.inc}>Increment</button>
+                    <button onClick={this.dec}>Decrement</button>
+                </section>
+            );
+        }
+    },
+);
+
+render(<Clicker />, document.getElementById('app'));
