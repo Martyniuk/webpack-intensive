f361ba8c367e54f596fe0ecac41f70bb7320742e
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2018-12-24 19:29:42 +0200

    → Lesson 3, checkpoint at: 19:42:10

---
+++ scripts/webpack/config/webpack.common.1.js
@@ -0,0 +1,38 @@
+// Core
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { HotModuleReplacementPlugin } = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+// Constants
+const { SOURCE, BUILD, PROJECT_ROOT } = require('../constants');
+
+module.exports = () => {
+    return {
+        entry:  [ 'webpack-hot-middleware/client?reload=true&quiet=true', SOURCE ],
+        output: {
+            path:     BUILD,
+            filename: 'bundle.js',
+        },
+        mode:    'none',
+        devtool: false,
+        module:  {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use:  [ 'style-loader', 'css-loader' ],
+                },
+            ],
+        },
+        plugins: [
+            new CleanWebpackPlugin([ 'build' ], {
+                root: PROJECT_ROOT,
+            }),
+            new HtmlWebpackPlugin({
+                title:    'Learn webpack',
+                template: './static/template.html',
+                favicon:  './static/favicon.ico',
+            }),
+            new HotModuleReplacementPlugin(),
+        ],
+    };
+};
