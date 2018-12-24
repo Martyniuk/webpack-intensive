6c9a573a834aa193ff4f4660025fee82ffaca57f
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2018-12-24 19:29:42 +0200

    → Lesson 3, checkpoint at: 19:59:58

--- scripts/webpack/config/webpack.dev.js
+++ scripts/webpack/config/webpack.dev.js
@@ -1,14 +1,10 @@
 // Core
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { HotModuleReplacementPlugin } = require('webpack');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const merge = require('webpack-merge');
 
 // Config
 const getCommonConfig = require('./webpack.common');
-
-// Constants
-const { PROJECT_ROOT } = require('../constants');
 
 module.exports = () => {
     return merge(getCommonConfig(), {
