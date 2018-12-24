8bba238bceef89cde2888d1e8f86d4cf13eac25e
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2018-12-24 19:29:42 +0200

    → Lesson 3, checkpoint at: 20:06:38

--- source/index.js
+++ source/index.js
@@ -1,14 +1,14 @@
-import './theme/init.css';
-import component from './simple-components/dom';
+// import './theme/init.css';
+// import component from './simple-components/dom';
 
-let element = component();
+// let element = component();
 
-document.body.appendChild(element);
+// document.body.appendChild(element);
 
-if (module.hot) {
-    module.hot.accept('./simple-components/dom', function() {
-        document.body.removeChild(element);
-        element = component();
-        document.body.appendChild(element);
-    });
-}
+// if (module.hot) {
+//     module.hot.accept('./simple-components/dom', function() {
+//         document.body.removeChild(element);
+//         element = component();
+//         document.body.appendChild(element);
+//     });
+// }
