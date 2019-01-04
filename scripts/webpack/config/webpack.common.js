c49b318c46ef5e61ddc9a5ba599e9d9cbad1bbb0
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-01-04 19:01:07 +0200

    → Lesson 4, checkpoint at: 20:02:41

--- scripts/webpack/config/webpack.common.js
+++ scripts/webpack/config/webpack.common.js
@@ -1,64 +1,62 @@
 // Core
+import merge from 'webpack-merge';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import env from 'postcss-preset-env';
 
 // Constants
 import { SOURCE, BUILD } from '../constants';
 
+// Webpack Modules
+import { loadJavaScript } from '../modules';
+
 export default () => {
-    return {
-        entry:  [ SOURCE ],
-        output: {
-            path:     BUILD,
-            filename: 'bundle.js',
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    use:  {
-                        loader:  'babel-loader',
-                        options: {
-                            compact: false, // 'auto' === true
-                        },
+    return merge(
+        {
+            entry:  [ SOURCE ],
+            output: {
+                path:     BUILD,
+                filename: 'bundle.js',
+            },
+            module: {
+                rules: [
+                    {
+                        test: /\.css$/,
+                        use:  [
+                            'style-loader',
+                            {
+                                loader:  'css-loader',
+                                options: {
+                                    modules:        true,
+                                    localIdentName:
+                                        '[path][name]__[local]--[hash:base64:5]',
+                                },
+                            },
+                            {
+                                loader:  'postcss-loader',
+                                options: {
+                                    plugins: [
+                                        // postcss-font-magician
+                                        // первый плагин
+                                        // второй плагин
+                                        env({
+                                            stage: 0,
+                                        }),
+                                        // последний плагин
+                                    ],
+                                },
+                            },
+                        ],
                     },
-                },
-                {
-                    test: /\.css$/,
-                    use:  [
-                        'style-loader',
-                        {
-                            loader:  'css-loader',
-                            options: {
-                                modules:        true,
-                                localIdentName:
-                                    '[path][name]__[local]--[hash:base64:5]',
-                            },
-                        },
-                        {
-                            loader:  'postcss-loader',
-                            options: {
-                                plugins: [
-                                    // postcss-font-magician
-                                    // первый плагин
-                                    // второй плагин
-                                    env({
-                                        stage: 0,
-                                    }),
-                                    // последний плагин
-                                ],
-                            },
-                        },
-                    ],
-                },
+                ],
+            },
+            plugins: [
+                new HtmlWebpackPlugin({
+                    title:    'Learn webpack',
+                    template: './static/template.html',
+                    favicon:  './static/favicon.ico',
+                }),
             ],
         },
-        plugins: [
-            new HtmlWebpackPlugin({
-                title:    'Learn webpack',
-                template: './static/template.html',
-                favicon:  './static/favicon.ico',
-            }),
-        ],
-    };
+        loadJavaScript(),
+    );
 };
