7e9ddec246adbb89c8c0b2f358df94c7464ca4ee
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-01-04 19:01:07 +0200

    → Lesson 4, checkpoint at: 20:05:02

---
+++ scripts/webpack/modules/assets.js
@@ -0,0 +1,15 @@
+export const loadJavaScript = () => ({
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use:  {
+                    loader:  'babel-loader',
+                    options: {
+                        compact: false, // 'auto' === true
+                    },
+                },
+            },
+        ],
+    },
+});
