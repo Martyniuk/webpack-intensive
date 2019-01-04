0dbb4523195276752ef1056c19e279a3b72dd3c2
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-01-04 19:01:07 +0200

    → Lesson 4, checkpoint at: 21:03:41

--- source/react-component/SkillMeter/index.js
+++ source/react-component/SkillMeter/index.js
@@ -42,7 +42,7 @@ const SkillMeter = hot(module)(
                             onClick = { this.dec }
                         />
                     </div>
-                    <ReactLogoSvg with = { 50 } />
+                    <ReactLogoSvg width = { 50 } />
                 </section>
             );
         }
