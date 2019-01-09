cd02b7f7c47e47eaac29db5f58d56f8c75313d25
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-01-09 19:36:29 +0200

    → Lesson 5, checkpoint at: 20:33:13

--- scripts/webpack/modules/utils.js
+++ scripts/webpack/modules/utils.js
@@ -1,6 +1,17 @@
 // Core
 import { DefinePlugin } from 'webpack';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
 export const defineEnvVariables = (variables = {}) => ({
     plugins: [ new DefinePlugin(variables) ],
+});
+
+export const setupBuildAnalysis = () => ({
+    plugins: [
+        new BundleAnalyzerPlugin({
+            analyzerMode:      'disabled',
+            openAnalyzer:      false,
+            generateStatsFile: true,
+        }),
+    ],
 });
