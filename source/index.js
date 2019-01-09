015de75f79cd02710c20df317e0ba649cc93dc50
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-01-09 19:36:29 +0200

    → Lesson 5, checkpoint at: 20:31:05

--- source/index.js
+++ source/index.js
@@ -15,11 +15,3 @@
 
 import './javascript';
 // import './react-component/SkillMeter';
-
-if (process.env.NODE_ENV === 'development') {
-    // что-то делать, или не делать
-}
-
-console.log('→ __DEV__', __DEV__);
-console.log('→ __PROD__', __PROD__);
-console.log('→ __ENV__', __ENV__);
