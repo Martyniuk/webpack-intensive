29cec76b76216c1df4114342f47cbe6fa036660c
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-01-09 19:36:29 +0200

    → Lesson 5, checkpoint at: 20:34:55

--- scripts/webpack/config/webpack.prod.js
+++ scripts/webpack/config/webpack.prod.js
@@ -5,17 +5,24 @@ import merge from 'webpack-merge';
 // Config
 import getCommonConfig from './webpack.common';
 
+// Webpack Modules
+import * as modules from '../modules';
+
 // Constants
 import { PROJECT_ROOT } from '../constants';
 
 export default () => {
-    return merge(getCommonConfig(), {
-        mode:    'none', // production
-        devtool: false,
-        plugins: [
-            new CleanWebpackPlugin([ 'build' ], {
-                root: PROJECT_ROOT,
-            }),
-        ],
-    });
+    return merge(
+        getCommonConfig(),
+        {
+            mode:    'none', // production
+            devtool: false,
+            plugins: [
+                new CleanWebpackPlugin([ 'build' ], {
+                    root: PROJECT_ROOT,
+                }),
+            ],
+        },
+        modules.setupBuildAnalysis(),
+    );
 };
