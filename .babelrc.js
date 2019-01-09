e022bae322d4d17d85a5fb89d12ba635eeea890c
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-01-09 19:36:29 +0200

    → Lesson 5, checkpoint at: 21:37:51

--- .babelrc.js
+++ .babelrc.js
@@ -20,7 +20,7 @@ module.exports = api => {
                     useBuiltIns: 'usage', // более прокачаный транспайлинг
                     spec: true, // более строгие проверки, более медленный код, более стабильный код
                     loose: false, // противоположность loose ↑
-                    modules: 'cjs', // false === ECMAScript modules
+                    modules: false, // false === ECMAScript modules
                 },
             ],
         ],
