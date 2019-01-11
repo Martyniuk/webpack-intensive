38b42279157db05ae9c8e7cdcee45795f673d980
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-01-11 19:20:56 +0200

    → Lesson 6, checkpoint at: 21:19:03

--- scripts/webpack/modules/utils.js
+++ scripts/webpack/modules/utils.js
@@ -10,6 +10,14 @@ export const defineEnvVariables = (variables = {}) => ({
     plugins: [ new DefinePlugin(variables) ],
 });
 
+export const cleanBuildDirectory = () => ({
+    plugins: [
+        new CleanWebpackPlugin([ 'build' ], {
+            root: PROJECT_ROOT,
+        }),
+    ],
+});
+
 export const setupBuildAnalysis = () => ({
     plugins: [
         new BundleAnalyzerPlugin({
