073d0f0186609a24cad40657cea9ca6ae67d2ef5
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-01-11 19:20:56 +0200

    → Lesson 6, checkpoint at: 21:22:09

--- scripts/webpack/modules/utils.js
+++ scripts/webpack/modules/utils.js
@@ -34,7 +34,7 @@ export const setupStaticServing = () => ({
         new CopyWebpackPlugin([
             {
                 from: `${STATIC}/CI/now.json`,
-                to:   `${BUILD}/package.json`,
+                to:   `${BUILD}/now.json`,
                 },
         ]),
     ],
