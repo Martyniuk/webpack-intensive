1b1cdc87f64c5ff388908434a2ab321e3d95a636
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-01-11 19:20:56 +0200

    → Lesson 6, checkpoint at: 19:46:39

---
+++ scripts/webpack/modules/styles.js
@@ -0,0 +1,47 @@
+// Core
+import env from 'postcss-preset-env';
+
+const loadPostCss = ({ sourceMap = false } = { sourceMap: false }) => {
+    const plugins = [
+        env({
+            stage: 0,
+        }),
+    ];
+
+    return {
+        loader:  'postcss-loader',
+        options: {
+            plugins,
+            sourceMap,
+        },
+    };
+};
+
+const loadCss = ({ sourceMap = false } = { sourceMap: false }) => ({
+    loader:  'css-loader',
+    options: {
+        modules:        true,
+        importLoaders:  1,
+        localIdentName: '[path][name]__[local]--[hash:base64:5]',
+        sourceMap,
+    },
+});
+
+export const loadDevCss = () => ({
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use:  [
+                    'style-loader',
+                    loadCss({ sourceMap: true }),
+                    loadPostCss({ sourceMap: true }),
+                    // sass-loader
+                    // sourceMap: true,
+                ],
+            },
+        ],
+    },
+});
+
+const loadProdCss = () => {};
