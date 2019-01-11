6db11bc3344ffee9305682ed7a5b18288a5bc403
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-01-11 19:20:56 +0200

    → Lesson 6, checkpoint at: 19:40:02

--- scripts/webpack/config/webpack.dev.js
+++ scripts/webpack/config/webpack.dev.js
@@ -5,11 +5,18 @@ import merge from 'webpack-merge';
 // Config
 import getCommonConfig from './webpack.common';
 
+// Webpack Modules
+import * as modules from '../modules';
+
 export default () => {
-    return merge(getCommonConfig(), {
-        entry:   [ 'webpack-hot-middleware/client?reload=true&quiet=true' ],
-        mode:    'development',
-        devtool: 'cheap-module-eval-source-map',
-        plugins: [ new HotModuleReplacementPlugin() ],
-    });
+    return merge(
+        getCommonConfig(),
+        {
+            entry:   [ 'webpack-hot-middleware/client?reload=true&quiet=true' ],
+            mode:    'development',
+            devtool: 'cheap-module-eval-source-map',
+            plugins: [ new HotModuleReplacementPlugin() ],
+        },
+        modules.loadDevCss(),
+    );
 };
