edafd08265447e693f1dc34af3d7b61c005cfbd2
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-01-11 19:20:56 +0200

    → Lesson 6, checkpoint at: 21:21:10

--- scripts/webpack/modules/utils.js
+++ scripts/webpack/modules/utils.js
@@ -1,6 +1,7 @@
 // Core
 import { DefinePlugin } from 'webpack';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import CleanWebpackPlugin from 'clean-webpack-plugin';
 import CopyWebpackPlugin from 'copy-webpack-plugin';
 
 // Instruments
