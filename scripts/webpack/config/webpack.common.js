45304df2cb82d4de2eeda53b2288ead4b9418ac9
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-01-11 19:20:56 +0200

    → Lesson 6, checkpoint at: 20:04:39

--- scripts/webpack/config/webpack.common.js
+++ scripts/webpack/config/webpack.common.js
@@ -15,7 +15,7 @@ export default () => {
             entry:  [ SOURCE ],
             output: {
                 path:     BUILD,
-                filename: 'js/bundle.js',
+                filename: 'js/[name].js',
             },
         },
         modules.setupHtml(),
