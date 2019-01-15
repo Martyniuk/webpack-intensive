ba38ca1700803ee378afa43a282f1ab281d1b489
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-01-15 19:24:16 +0200

    → Lesson 7, checkpoint at: 19:50:05

--- source/simple-components/dom.js
+++ source/simple-components/dom.js
@@ -6,9 +6,12 @@ export default (text = 'Хеллоу гайз!!! 🎉🎉🎉 🔥🔥🔥') => 
     /* Code splitting */
     element.addEventListener('click', async () => {
         // 1. индикатор загрузки
+        element.innerHTML = 'Подготовка....';
+        await (() => new Promise((resolve) => setTimeout(resolve, 2000)))(); // ждём 2 секунды
         element.innerHTML = 'Загружаю....';
 
-        await (() => new Promise((resolve) => setTimeout(resolve, 2000)))(); // ждём 2 секунды
+
+
 
         // 2. загрузить split point
         // 3. добавить в элемент загруженный текст
