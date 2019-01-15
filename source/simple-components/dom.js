827222bb14e63e27249cf73f203345d432831e6b
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-01-15 19:24:16 +0200

    → Lesson 7, checkpoint at: 19:49:03

--- source/simple-components/dom.js
+++ source/simple-components/dom.js
@@ -3,5 +3,13 @@ export default (text = 'Хеллоу гайз!!! 🎉🎉🎉 🔥🔥🔥') => 
 
     element.innerHTML = text;
 
+    /* Code splitting */
+    element.addEventListener('click', async () => {
+        element.innerHTML = 'Загружаю....';
+        // 1. индикатор загрузки
+        // 2. загрузить split point
+        // 3. добавить в элемент загруженный текст
+    });
+
     return element;
 };
