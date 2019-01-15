b9629500bdf3d92aec14f0cc019f613325cd6c84
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-01-15 19:24:16 +0200

    → Lesson 7, checkpoint at: 21:46:30

--- scripts/webpack/config/webpack.common.js
+++ scripts/webpack/config/webpack.common.js
@@ -7,11 +7,9 @@ import { SOURCE, BUILD } from '../constants';
 // Webpack Modules
 import * as modules from '../modules';
 
-console.log('→ 1 ', );
 export default () => {
     const { NODE_ENV } = process.env;
 
-console.log('→ 2 ', );
     const DEV = NODE_ENV === 'development';
 
     return merge(
