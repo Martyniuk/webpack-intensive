84b83f6554781a5d6a6139b344db387c66c11454
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-01-15 19:24:16 +0200

    → Lesson 7, checkpoint at: 20:50:21

--- scripts/webpack/config/webpack.common.js
+++ scripts/webpack/config/webpack.common.js
@@ -10,14 +10,18 @@ import * as modules from '../modules';
 export default () => {
     const { NODE_ENV } = process.env;
 
+    const DEV = NODE_ENV === 'development';
+
     return merge(
         {
             entry:  [ SOURCE ],
             output: {
                 path:          BUILD,
-                filename:      'js/[name].[contenthash:5].js',
-                chunkFilename: 'js/[name].[contenthash:5].js',
-                publicPath:    '/',
+                filename:      DEV ? 'js/[name].js' : 'js/[name].[contenthash:5].js',
+                chunkFilename: DEV
+                    ? 'js/[name].js'
+                    : 'js/[name].[contenthash:5].js',
+                publicPath: '/',
             },
         },
         modules.setupHtml(),
