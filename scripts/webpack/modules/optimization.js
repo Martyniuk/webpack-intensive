ed5af52df180ef416d5e2109d8347da8071e516d
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-01-15 19:24:16 +0200

    → Lesson 7, checkpoint at: 19:41:51

--- scripts/webpack/modules/optimization.js
+++ scripts/webpack/modules/optimization.js
@@ -53,7 +53,7 @@ export const optimizeBuild = () => {
             // Эта опция включена всегда. Когфигурируется в SplitChunksPlugin.
             splitChunks: {
                 // Режим разделения кода. По-умолчанию — async.
-                chunks: 'async',
+                chunks: 'async', // 'initial', 'all'
             },
             // TODO: для нас runtimeChunk
         },
