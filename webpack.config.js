0501fb5114a35907282519d4e988dd7b346ee2f9
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-05 15:43:14 +0200

    → Lesson 1. Checkpoint at: 17:42:43

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,13 @@
+const delay = (timeout = 1000) =>
+    new Promise(resolve => setTimeout(resolve, timeout));
+
 /**
  * Типы конфигов вебпак:
  * Function
  * Object
  * Promise
  */
-
-const delay = (timeout = 1000) =>
-    new Promise(resolve => setTimeout(resolve, timeout));
-
 module.exports = () => {
-
     return {
         mode: 'none',
         devtool: false,
