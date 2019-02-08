50d7bde6e8cc989b59fa45a3a3d1ab4152116689
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-08 19:37:51 +0200

    → Lesson 2. Checkpoint at: 21:21:49

--- scripts/webpack/start.js
+++ scripts/webpack/start.js
@@ -17,7 +17,7 @@ const getConfig = require('./webpack.config');
 // Hot reloading:
 // 1. ✓ настроить на сервере
 // 2. ✓ настроить на клиенте
-// 3. настроить в исходном коде
+// 3. ✓ настроить в исходном коде
 
 // Constants
 const { HOST, PORT } = require('./constants');
