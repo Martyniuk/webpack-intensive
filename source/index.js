21fbb08d8a92d4a341002c3a507b80e478e134f5
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-08 19:37:51 +0200

    → Lesson 2. Checkpoint at: 21:26:43

--- source/index.js
+++ source/index.js
@@ -7,8 +7,7 @@ document.body.appendChild(element);
 // TODO: hot reloading на React
 // 1. react-hot-loader: babel-плагин
 // 2. react-hot-loader: исходный код
-
-hot(module)(component);
+// hot(module)(component);
 
 if (module.hot) {
     module.hot.accept('./simple-components/dom', function() {
