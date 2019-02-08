c7a50bfdfbcf70ab4170ee26c8fdb3d8c63db37d
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-08 19:37:51 +0200

    → Lesson 2. Checkpoint at: 21:20:15

--- source/index.js
+++ source/index.js
@@ -4,6 +4,8 @@ import './theme/main.css';
 const element = component();
 document.body.appendChild(element);
 
+// hot(module)(component);
+
 if (module.hot) {
     module.hot.accept('./simple-components/dom', function() {
         document.body.removeChild(element);
