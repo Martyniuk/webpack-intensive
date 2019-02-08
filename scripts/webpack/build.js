d5e24a7855291a3df742990562a881e199fada6c
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-08 19:37:51 +0200

    → Lesson 2. Checkpoint at: 20:05:45

--- scripts/webpack/build.js
+++ scripts/webpack/build.js
@@ -2,8 +2,6 @@
 const webpack = require('webpack');
 const chalk = require('chalk'); // Раскрашивает консоль
 
-console.log('→ ', chalk.bgRed.blue('START!'));
-
 // 1. webpack
 // 2. конфигурация ⚙️
 // 3. запуск 🚀
