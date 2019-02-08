ec7c3f584e4795b2b4b41aaf41b70dfb0ee158b6
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-08 19:37:51 +0200

    → Lesson 2. Checkpoint at: 21:22:22

--- scripts/webpack/webpack.config.js
+++ scripts/webpack/webpack.config.js
@@ -1,4 +1,5 @@
 // Core
+const { HotModuleReplacementPlugin } = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
@@ -49,6 +50,7 @@ module.exports = () => {
                 favicon:  './static/favicon.ico',
             }),
             new CleanWebpackPlugin([ BUILD_DIRECTORY ], cleanOptions),
+            new HotModuleReplacementPlugin(),
         ],
     };
 };
