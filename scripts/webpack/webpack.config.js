96322baf3863cb43f2f3c85fd06ef4dddf36be2f
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-08 19:37:51 +0200

    → Lesson 2. Checkpoint at: 19:58:41

--- scripts/webpack/webpack.config.js
+++ scripts/webpack/webpack.config.js
@@ -3,7 +3,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 // Constants
-const { BUILD_DIRECTORY, PROJECT_ROOT } = require('./constants');
+const {
+    PROJECT_ROOT,
+    SOURCE_DIRECTORY,
+    BUILD_DIRECTORY,
+} = require('./constants');
 
 // the clean options to use
 const cleanOptions = {
@@ -19,6 +23,10 @@ const cleanOptions = {
  */
 module.exports = () => {
     return {
+        entry:  SOURCE_DIRECTORY,
+        output: {
+            path: BUILD_DIRECTORY,
+        },
         mode:    'none',
         devtool: false,
         plugins: [
