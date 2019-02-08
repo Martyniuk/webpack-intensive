c8b1a0de350c0b3d2e4047a07f52239c00dd895f
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-08 19:37:51 +0200

    → Lesson 2. Checkpoint at: 21:00:49

---
+++ scripts/webpack/build.1.js
@@ -0,0 +1,57 @@
+// 1. ✓ webpack
+// 2. ✓ конфигурация ⚙️
+// 3. ✓ создать компайлер webpack
+// 4. ✓ запуск 🚀
+
+// Core
+const webpack = require('webpack');
+const chalk = require('chalk'); // Раскрашивает консоль
+
+// Config
+const getConfig = require('./webpack.config');
+
+const compiler = webpack(getConfig());
+
+// compiler.hooks.beforeRun.tap({ name: 'start' }, () => {
+//     console.log('→ compilation started');
+// });
+// compiler.hooks.done.tap({ name: 'done' }, () => {
+//     console.log('→ compilation completed');
+// });
+
+compiler.run((error, stats) => {
+    if (error) {
+        // ошибка конфигурации
+        console.error(error.stack || error);
+
+        if (error.details) {
+            console.error(error.details);
+        }
+
+        return null;
+    }
+
+    const info = stats.toString({
+        hash:        true,
+        colors:      true,
+        version:     true,
+        env:         true,
+        modules:     false,
+        entrypoints: false,
+    });
+
+    console.log(chalk.greenBright('✓ Build completed'));
+    console.log(info);
+
+    if (stats.hasErrors()) {
+        // ошибка во время компиляции (битый импорт, ошибка синтаксиса, etc)
+        console.log(chalk.redBright('→ Error!'));
+        console.error(info);
+    }
+
+    if (stats.hasWarnings()) {
+        // ворнинг во время компиляции
+        console.log(chalk.yellowBright('→ Warning!'));
+        console.warn(info);
+    }
+});
