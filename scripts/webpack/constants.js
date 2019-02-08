e2b52b4b999efd2a4fd8273b0f2cf1d511d88e88
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-08 19:37:51 +0200

    → Lesson 2. Checkpoint at: 19:55:28

--- scripts/webpack/constants.js
+++ scripts/webpack/constants.js
@@ -1,6 +1,7 @@
 // Core
 const { resolve } = require('path');
+const { path: PROJECT_ROOT } = require('app-root-path');
 
 // the path(s) that should be cleaned
-exports.PROJECT_ROOT = resolve(__dirname, '../..');
+exports.PROJECT_ROOT = PROJECT_ROOT;
 exports.BUILD_DIRECTORY = resolve(__dirname, '../../dist');
