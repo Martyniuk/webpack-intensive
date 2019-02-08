4abdf65a0e73aa2b6fbda2c6d0483bbc74a9cff0
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-08 19:37:51 +0200

    → Lesson 2. Checkpoint at: 20:25:14

--- scripts/webpack/build.js
+++ scripts/webpack/build.js
@@ -12,12 +12,12 @@ const getConfig = require('./webpack.config');
 
 const compiler = webpack(getConfig());
 
-compiler.hooks.beforeRun.tap({ name: 'start' }, () => {
-    console.log('→ compilation started');
-});
-compiler.hooks.done.tap({ name: 'done' }, () => {
-    console.log('→ compilation completed');
-});
+// compiler.hooks.beforeRun.tap({ name: 'start' }, () => {
+//     console.log('→ compilation started');
+// });
+// compiler.hooks.done.tap({ name: 'done' }, () => {
+//     console.log('→ compilation completed');
+// });
 
 compiler.run((error, stats) => {
     if (error) {
