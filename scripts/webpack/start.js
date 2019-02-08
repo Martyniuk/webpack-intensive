35dc69a9f50099bbc1c4b9ab5067a8efd457cd3b
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-08 19:37:51 +0200

    → Lesson 2. Checkpoint at: 21:09:15

--- scripts/webpack/start.js
+++ scripts/webpack/start.js
@@ -7,9 +7,31 @@
 
 // Core
 const webpack = require('webpack');
+const DevServer = require('webpack-dev-server');
+const hot = require('webpack-hot-middleware');
 const chalk = require('chalk'); // Раскрашивает консоль
 
 // Config
 const getConfig = require('./webpack.config');
 
+// Constants
+const { HOST, PORT } = require('./constants');
+
 const compiler = webpack(getConfig());
+
+const server = new DevServer(compiler, {
+    host:               HOST,
+    port:               PORT,
+    historyApiFallback: true,
+    overlay:            true,
+    quiet:              true,
+    clientLogLevel:     'none',
+    noInfo:             true,
+    after:              (app) => {
+        app.use(
+            hot(compiler, {
+                log: false,
+            }),
+        );
+    },
+});
