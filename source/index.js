ebea39aa1ebb687fb9891f24bfc98f97b741d35f
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-08 19:37:51 +0200

    → Lesson 2. Checkpoint at: 21:19:26

--- source/index.js
+++ source/index.js
@@ -3,3 +3,11 @@ import './theme/main.css';
 
 const element = component();
 document.body.appendChild(element);
+
+if (module.hot) {
+    module.hot.accept('./simple-components/dom', function() {
+        document.body.removeChild(element);
+        element = component();
+        document.body.appendChild(element);
+    });
+}
