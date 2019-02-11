9e95c9129e84999d4e92d982ac35cb19afde9d81
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-11 19:41:42 +0200

    → Lesson 3. Checkpoint at: 21:23:09

--- scripts/webpack/config/webpack.common.js
+++ scripts/webpack/config/webpack.common.js
@@ -46,7 +46,33 @@ module.exports = () => {
                                     // цепочка плагинов postcss
 
                                     env({
-                                        stage: 0, // default: stage 2
+                                        stage:    0, // default: stage 2
+                                        features: {
+                                            'custom-media-queries': {
+                                                importFrom: [
+                                                    {
+                                                        customMedia: {
+                                                            '--phonePortrait':
+                                                                '(width <= 414px)',
+                                                            '--phoneLandscape':
+                                                                '(width >= 415px) and (width <= 667px)',
+                                                            '--tabletPortrait':
+                                                                '(width >= 668px) and (width <= 768px)',
+                                                            '--tabletLandscape':
+                                                                '(width >= 769px) and (width <= 1024px)',
+                                                            '--desktopS':
+                                                                '(width >= 1025px) and (width <= 1366px)',
+                                                            '--desktopM':
+                                                                '(width >= 1367px) and (width <= 1680px)',
+                                                            '--desktopL':
+                                                                '(width >= 1681px) and (width <= 1920px)',
+                                                            '--desktopXL':
+                                                                '(width >= 1921px)',
+                                                        },
+                                                    },
+                                                ],
+                                            },
+                                        },
                                     }),
                                 ],
                             },
