b4bcc83a8c5fa46bc57f561d31e3bab22dcd79f8
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-11 19:41:42 +0200

    → Lesson 3. Checkpoint at: 20:00:15

--- scripts/webpack/start.js
+++ scripts/webpack/start.js
@@ -19,7 +19,7 @@ const hot = require('webpack-hot-middleware');
 const chalk = require('chalk'); // Раскрашивает консоль
 
 // Config
-const getConfig = require('./config/webpack.common');
+const getConfig = require('./config/webpack.dev');
 
 // Utils
 const { choosePort } = require('./utils');
