300b93f74d317d91850a9674ecbf0366902f7340
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-11 19:41:42 +0200

    → Lesson 3. Checkpoint at: 19:44:46

---
+++ scripts/webpack/webpack.config.2.js
@@ -0,0 +1,56 @@
+// Core
+const { HotModuleReplacementPlugin } = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+// Constants
+const {
+    PROJECT_ROOT,
+    SOURCE_DIRECTORY,
+    BUILD_DIRECTORY,
+} = require('./constants');
+
+// the clean options to use
+const cleanOptions = {
+    verbose: true,
+    root:    PROJECT_ROOT,
+};
+
+/**
+ * Типы конфигов вебпак:
+ * Function
+ * Object
+ * Promise
+ */
+module.exports = () => {
+    return {
+        mode:    'none',
+        devtool: false,
+        entry:   [
+            'webpack-hot-middleware/client?reload=true&quiet=true',
+            SOURCE_DIRECTORY,
+        ],
+        output: {
+            path:     BUILD_DIRECTORY,
+            filename: 'bundle.js',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use:  [ 'style-loader', 'css-loader' ],
+                },
+            ],
+        },
+        plugins: [
+            // Каждый плагин — это конструктор
+            new HtmlWebpackPlugin({
+                template: './static/template.html',
+                title:    'Изучаем вебпак! 🚀',
+                favicon:  './static/favicon.ico',
+            }),
+            new CleanWebpackPlugin([ BUILD_DIRECTORY ], cleanOptions),
+            new HotModuleReplacementPlugin(),
+        ],
+    };
+};
