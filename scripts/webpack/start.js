e838fbc001dcec07141bc2b0f8844f22f1a3f112
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-11 19:41:42 +0200

    → Lesson 3. Checkpoint at: 19:41:43

--- scripts/webpack/start.js
+++ scripts/webpack/start.js
@@ -1,9 +1,16 @@
+// Node webpack dev server
 // 1. ✓ webpack
 // 2. ✓ webpack-dev-server (express + webpack-dev-middleware + много хелперов)
 // 3. ✓ webpack-hot-middleware
 // 4. ✓ конфигурация ⚙️
 // 5. ✓ создать компайлер webpack
 // 6. ✓ запуск 🚀
+
+// Hot reloading:
+// 1. ✓ настроить на сервере
+// 2. ✓ настроить на клиенте
+// 3. ✓ настроить в вебпаке :)
+// 4. ✓ настроить в исходном коде
 
 // Core
 const webpack = require('webpack');
@@ -14,38 +21,52 @@ const chalk = require('chalk'); // Раскрашивает консоль
 // Config
 const getConfig = require('./webpack.config');
 
-// Hot reloading:
-// 1. ✓ настроить на сервере
-// 2. ✓ настроить на клиенте
-// 3. ✓ настроить в вебпаке :)
-// 4. ✓ настроить в исходном коде
+// Utils
+const { choosePort } = require('./utils');
 
 // Constants
 const { HOST, PORT } = require('./constants');
 
 const compiler = webpack(getConfig());
 
-const server = new DevServer(compiler, {
-    host:               HOST,
-    port:               PORT,
-    historyApiFallback: true,
-    overlay:            true,
-    quiet:              true,
-    clientLogLevel:     'none',
-    noInfo:             true,
-    after:              (app) => {
-        app.use(
-            hot(compiler, {
-                log: false,
-            }),
-        );
-    },
-});
+(async () => {
+    try {
+        const choosenPort = await choosePort(PORT);
 
-server.listen(PORT, HOST, () => {
-    console.log(
-        `${chalk.greenBright('→ Server listening on')} ${chalk.blueBright(
-            `http://${HOST}:${PORT}`,
-        )}`,
-    );
-});
+        if (!choosenPort) {
+            console.log(
+                chalk.yellowBright('→ It\'s impossible to run the app :('),
+            );
+
+            return null;
+        }
+
+        const server = new DevServer(compiler, {
+            host:               HOST,
+            port:               choosenPort,
+            historyApiFallback: true,
+            overlay:            true,
+            quiet:              true,
+            clientLogLevel:     'none',
+            noInfo:             true,
+            after:              (app) => {
+                app.use(
+                    hot(compiler, {
+                        log: false,
+                    }),
+                );
+            },
+        });
+
+        server.listen(choosenPort, HOST, () => {
+            console.log(
+                `${chalk.greenBright(
+                    '→ Server listening on',
+                )} ${chalk.blueBright(`http://${HOST}:${choosenPort}`)}`,
+            );
+        });
+    } catch (error) {
+        console.log(chalk.redBright('→ Error!'));
+        console.error(error.message || error);
+    }
+})();
---
+++ scripts/webpack/utils.js
@@ -0,0 +1,34 @@
+const chalk = require('chalk');
+const detectPort = require('detect-port-alt');
+const inquirer = require('inquirer');
+
+exports.choosePort = async (defaultPort) => {
+    try {
+        const port = await detectPort(defaultPort);
+        if (port === defaultPort) {
+            return defaultPort;
+        }
+
+        const message = `Port ${defaultPort} is already in use.`;
+
+        if (process.stdout.isTTY) {
+            const questionName = 'changePort';
+            const question = {
+                type: 'confirm',
+                name: questionName,
+                message: chalk.yellowBright(`${message} Do you want to run the app on another port?`),
+                default: true,
+            };
+
+            const result = await inquirer.prompt(question);
+            return result[questionName] ? port : null;
+        }
+
+        console.log(chalk.redBright(`→ ${message}`));
+    } catch (err) {
+        console.log(chalk.redBright('→ Error!'));
+        console.error(err.message || err)
+    }
+
+    return null
+};
